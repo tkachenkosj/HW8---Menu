2ae723d0fd70db9016fa577854b33ff8b7aab78d
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-11-26 10:00:17 +0300

    Initial Commit

---
+++ HW8 - Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW8 - Menu
+//
+//  Created by Сергей Ткаченко on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
